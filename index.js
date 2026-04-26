e6b9bfb8ed850bfa3ada40608ad208c9ea24c0c0
KarmicVoid <karmic132@protonmail.com>
2026-04-26 17:00:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({ 
+const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds, 
-        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent
-    ] 
+    ]
 });
 
 client.on('ready', () => {
     console.log(`${client.user.tag} olarak giriş yapıldı ve bot şu an aktif!`);
 });
 
+client.on('messageCreate', (message) => {
+    if (message.author.bot) return;
+
+    if (message.content === '!panel') {
+        message.channel.send('Bilet paneli başarıyla oluşturuldu!');
+    }
+});
+
 client.login(process.env.DISCORD_TOKEN);
+
