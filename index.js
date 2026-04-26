6e1cd7bb658062c4825cb23ca37b6de1cf039103
KarmicVoid <karmic132@protonmail.com>
2026-04-26 16:30:26 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent
+    ] 
+});
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} olarak giriş yapıldı ve bot şu an aktif!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
